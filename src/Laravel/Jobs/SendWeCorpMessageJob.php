c51aea63321c35234b3c552eea7181fbb6ed5e2e
codelint <unotseeme@foxmail.com>
2021-11-29 15:23:58 +0800

    bug fix

--- src/Laravel/Jobs/SendWeCorpMessageJob.php
+++ src/Laravel/Jobs/SendWeCorpMessageJob.php
@@ -76,10 +76,16 @@ class SendWeCorpMessageJob implements ShouldQueue {
         $chat_ids = cache()->get('base.Service.Logger.wx.chat_ids', []);
         if (!Arr::has($chat_ids, $this->chat_id))
         {
-            if ($this->genChannel($this->chat_id, ['gzhang', 'dev']))
+            $user_ids = env('RINGO_WECORP_INIT_UIDS');
+            $user_ids = $user_ids ? explode(',', $user_ids) : [];
+            if (count($user_ids) > 0 && $this->genChannel($this->chat_id, $user_ids))
             {
                 $chat_ids[] = $this->chat_id;
                 cache()->put('base.Service.Logger.wx.chat_ids', $chat_ids);
+            }
+            else
+            {
+                return;
             }
         }
         $this->wx($this->message, $this->detail);
