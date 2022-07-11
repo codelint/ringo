65705760f4355e997a551909d64cfa8ca6465389
codelint <unotseeme@foxmail.com>
2022-07-11 11:41:20 +0800

    enhance

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,21 @@ Route::get('/ringo/message/view/{view_id}', function($view_id){
     $data = \Codelint\Ringo\Laravel\Facade\Cache::get($view_id, []);
     $detail =\Illuminate\Support\Arr::get($data, 'detail', $data);
     $extra = array();
+    $arr = [];
     foreach($detail as $k => $v){
         if(is_array($v))
         {
             $extra[$k] = $v;
         }
+        if(!\Illuminate\Support\Str::startsWith($k, '_'))
+        {
+            $arr[] = $v;
+        }
     }
 
     return view($data['view'] ?? 'ringo::sample.info')
         ->with('message', \Illuminate\Support\Arr::get($data, 'message', $view_id))
-        ->with('detail', $detail)
+        ->with('detail', $arr)
         ->with('extra', $extra)
         ->with('meta', \Illuminate\Support\Arr::get($data, 'meta', []));
 });
